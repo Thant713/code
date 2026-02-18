61dd3c2e51e67eb074036d6e343df2914e8bde43
t <59516322+Thant713@users.noreply.github.com>
2026-02-17 20:07:06 -0500

    a = .5 (b + h) complete

--- cppExercises/developingYourFirstProgram/1/triArea.cpp
+++ cppExercises/developingYourFirstProgram/1/triArea.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main() {
-    
+    double b, h, a;
+
+    cout << "Insert base: ";
+    cin >> b;
+    cout << "Insert height: ";
+    cin >> h;
+
+    a = 0.5 * (b + h);
+
+    cout << "The area of the triangle is: " << a << endl;
+
+    return 0;
 }
