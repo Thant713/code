15905659dd5335614acdb75d60b438a011e42551
t <59516322+Thant713@users.noreply.github.com>
2026-03-13 13:39:24 -0400

    tried git add -A :/

---
+++ cppExercises/csc111/l5/array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+/*Lab Exercise:  Creating an array of random numbers
+
+In this exercise, we will work with an array of 100 elements. The numbers loaded into the array will be generated using a randomizer function.  Once the array is loaded, generate the following statistics:
+
+    Print the numbers of the array in 5 rows of 20 numbers each
+    Minimum value
+    Maximum value
+    Total of all the numbers
+    Average of all the numbers
+    The number in the middle of the array
+    The number in the first element of the array
+    The number in the last element of the array
+    Count of odd numbers
+    Count of even numbers
+
+Note:  You might need to research how to use the randomizer function of C++.
+*/
