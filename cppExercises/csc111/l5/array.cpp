34140c814613cdff8ed4c3241613de3d1aafaa4d
t <59516322+Thant713@users.noreply.github.com>
2026-03-15 13:32:22 -0400

    l5

--- cppExercises/csc111/l5/array.cpp
+++ cppExercises/csc111/l5/array.cpp
@@ -1,14 +1,56 @@
+#include <climits>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 int main() {
+  srand(time(0));
+  int array[100], min(INT_MAX), max(INT_MIN), total(0),
+      count = sizeof(array) / sizeof(array[0]), evenCount(0), oddCount(0);
+  for (int i = 0; i < 100; i++) {
+    array[i] = rand() % 100 + 1;
 
-    return 0;
+    cout << array[i] << " ";
+    if ((i + 1) % 20 == 0) {
+      cout << endl;
+    }
+
+    if (array[i] < min) {
+      min = array[i];
+    }
+
+    if (array[i] > max) {
+      max = array[i];
+    }
+
+    if (array[i] % 2 == 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+    total += array[i];
+  }
+  cout << "Minimum value: " << min << "\nMaximum value: " << max
+       << "\nTotal of all the numbers: " << total
+       << "\nAverage of all the numbers: " << float(total) / count
+       << "\nThe numbers in the middle of the array: " << array[99 / 2] << " "
+       << array[100 / 2]
+       << "\nThe number in the first element of the array: " << array[0]
+       << "\nThe number in the last element of the array: " << array[99]
+       << "\nCount of odd numbers: " << oddCount
+       << "\nCount of even numbers: " << evenCount << endl;
+
+  return 0;
 }
+
+// notes to self: 9, 11, 12, 15, 37, 38, 40 hardcoded
 
 /*Lab Exercise:  Creating an array of random numbers
 
-In this exercise, we will work with an array of 100 elements. The numbers loaded into the array will be generated using a randomizer function.  Once the array is loaded, generate the following statistics:
+In this exercise, we will work with an array of 100 elements. The numbers loaded
+into the array will be generated using a randomizer function.  Once the array is
+loaded, generate the following statistics:
 
     Print the numbers of the array in 5 rows of 20 numbers each
     Minimum value
