991497d32344ff303cc212091559bae9440af48f
Thant <59516322+Thant713@users.noreply.github.com>
2026-04-02 19:14:22 -0400

    bootdev ch3 print a report

--- bootdev/1/main.py
+++ bootdev/1/main.py
@@ -1,15 +1,31 @@
-from stats import get_num_words, get_num_chars
-
-def main():
-    book_path = "../books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(f"Found {get_num_words(text)} total words")
-    
-    bookChars = get_num_chars(text)
-    print(f"Dictionary of characters and their counts: {bookChars}")
+from stats import get_num_words, get_num_chars, get_sorted_chars
 
 def get_book_text(path):
+    # turns a file path into txt file
     with open(path) as f:
         return f.read()
-    
+
+def print_report(book_path, numWords, all_char_count):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book_path}")
+  print("----------- Word Count ----------")
+  print(f"Found {numWords} total words")
+  print("--------- Character Count -------")
+  for dict in all_char_count:
+    if not dict["char"].isalpha():
+      continue
+    print(f"{dict["char"]}: {dict["num"]}")
+  print("============= END ===============")
+
+def main():
+  book_path = "../books/frankenstein.txt"
+  text = get_book_text(book_path)
+  bookChars = get_num_chars(text)
+  numWords = get_num_words(text)
+  all = get_sorted_chars(bookChars)
+  print_report(book_path, numWords, all)
+  # print(f"Dictionary of characters and their counts: {bookChars}")
+
 main()
+
+
--- bootdev/1/stats.py
+++ bootdev/1/stats.py
@@ -1,9 +1,11 @@
 def get_num_words(i): #i in this case == fullText
+    # returns the num of WORDS in a txt file
     split = i.split()
     num = len(split)
     return num
 
 def get_num_chars(fullText):
+    # returns a dictionary of CHARS w/ their counts in a txt file
     allChars = {}
     for char in fullText:
         char = char.lower()
@@ -12,3 +14,17 @@ def get_num_chars(fullText):
         else:
             allChars[char] = 1
     return allChars
+
+def sort_on(char_dict):
+    # returns the num's value from the dictionary, used as a sort key
+    return char_dict["num"]
+
+def get_sorted_chars(char_count_dictionary):
+    # returns a list of character dictionaries sorted from greatest to least count
+    all_char_count = []
+    for char in char_count_dictionary:
+        all_char_count.append({"char": char, "num": char_count_dictionary[char]})
+    all_char_count.sort(reverse=True, key=sort_on)
+
+    return all_char_count
+
