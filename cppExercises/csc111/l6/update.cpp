caea1ed3c00bc3d4482787f3783e7ce58a09f1f8
Thant <59516322+Thant713@users.noreply.github.com>
2026-04-20 20:03:54 -0400

    blp2

---
+++ cppExercises/csc111/l6/update.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+
+int main() {
+
+}
+/*Lab Exercise 6 - Update the WorldSeriesWinners.txt with newwinners.txt teams
+Instructions
+
+In Assignment 4, World Series Winners, the winners.txt was not updated since 2020.
+Write a C++ program to append the attached file to the end of the WorldSeriesWinners.txt file.
+You can either read from the old file WorldSeriesWinners.txt, and read the newwinners.txt file and create a new output  file,
+or open the old file as ofstream with an append option, and read the newwinners.txt file to add new records to the end of the file.
+Don't forget to close the files.
+
+The NewWinners.txt will be attached to this assignment.  To complete this assignment, attach your source code.*/
+
