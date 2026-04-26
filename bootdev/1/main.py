ba0d98e9fc82315c259ef18c756efc75cdc2aa55
tLaptop <59516322+Thant713@users.noreply.github.com>
2026-04-26 18:54:48 -0400

    laptop test

--- bootdev/1/main.py
+++ bootdev/1/main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_chars, get_sorted_chars
+import sys
 
 def get_book_text(path):
     # turns a file path into txt file
@@ -18,7 +19,10 @@ def print_report(book_path, numWords, all_char_count):
   print("============= END ===============")
 
 def main():
-  book_path = "../books/frankenstein.txt"
+  if sys.argv < 2:
+    print("")
+
+  book_path = sys.argv[1]
   text = get_book_text(book_path)
   bookChars = get_num_chars(text)
   numWords = get_num_words(text)
